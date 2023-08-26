17d47f1c29129dbd0394768a5d168bb4b5c35a13
Himanikuraware <kurawarehimani26@gmail.com>
2023-08-26 18:17:35 +0530

    Getting data from Mongodb.

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,33 @@
 import React from "react";
+import { MongoClient } from "mongodb";
 
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 10, 12345 Some City",
-    description: "This is a second meetup!",
-  },
-];
 
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-// This function will always run after the deployment.
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//   };
-// }
-
 // Fetching data for static pages. It will be called before our component this function will run during the build process.
 export async function getStaticProps() {
+  // We can use MongoClient here as Nextjs will not expose this code to the client.
+  const client = await MongoClient.connect(process.env.REACT_APP_MONGODB_URL);
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
     },
     // to use latest data without building it again we can use revalidate prop(Incremental Static Generation).
-    revalidate: 10, //number od seconds.
+    revalidate: 1, //number od seconds.
   };
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const addMeetuphandler = (newMeetup) => {
-    console.log(newMeetup);
+  const router = useRouter();
+
+  const addMeetuphandler = async (newMeetup) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(newMeetup),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
   };
   return <NewMeetupForm onAddMeetup={addMeetuphandler} />;
 };
