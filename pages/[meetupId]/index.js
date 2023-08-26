947a87eced3378e1d6f4cecc819a435f25769bdf
Himanikuraware <kurawarehimani26@gmail.com>
2023-08-26 17:10:15 +0530

    Added getStaticPaths.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetailsPage = () => {
+const MeetupDetailsPage = (props) => {
   return (
     <MeetupDetail
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
@@ -12,5 +12,41 @@ const MeetupDetailsPage = () => {
     />
   );
 };
+
+// We need getStaticPath function of we use getStaticProps in dynamic path page.
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "My meetup",
+        address: "Some street",
+        description: "The meetup description",
+      },
+    },
+  };
+}
 
 export default MeetupDetailsPage;
